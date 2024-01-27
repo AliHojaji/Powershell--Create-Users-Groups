20a7d230468f5936e113ccbb4b51df2be2156ba3
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 13:58:45 +0330

    Add files via upload

---
+++ Create Users & Groups.ps1
@@ -0,0 +1,11 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*------------------------------*--#
+#---> Manage Windows Server Core <---#
+#--*------------------------------*--#
+
+#Create User in ADDS
+New-ADUser -Name "username" -GivenName "user firstname" -Surname "user lastname" -DisplayName "user displayname" -SamAccountName "username" -UserPrincipalName "username@domain.com" -AccountPassword (ConvertTo-SecureString "password" -AsPlainText -Force) -Enabled $true
+
+#Create local User
+New-LocalUser -Name "username" -Description "user description" -FullName "user full name" -Password (ConvertTo-SecureString "password" -AsPlainText -Force)
